0f2c84e18d5e45b64cad9df9a6ee33b007eb0c18
MaksimMalygin <MaksimMalygin@gmail.com>
2020-10-21 22:18:57 +0400

    сделал сжатие js файла

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-var html = document.querySelector("html");
-var popupButtonOpen = document.querySelector("#popup-open-button");
-var popupButtonClose = document.querySelector("#popup-close-button");
-
-popupButtonOpen.addEventListener("click", function() {
-    html.classList.add("popup-open");
-    popupButtonClose.focus();
-});
-
-popupButtonClose.addEventListener("click", function() {
-    html.classList.remove("popup-open");
-});
-
-window.addEventListener("keydown", function(event) {
-    if (event.code === "Escape" && html.classList.contains("popup-open")) {
-        html.classList.remove("popup-open");
-    }
-})
+var html=document.querySelector("html"),popupButtonOpen=document.querySelector("#popup-open-button"),popupButtonClose=document.querySelector("#popup-close-button");popupButtonOpen.addEventListener("click",function(){html.classList.add("popup-open"),popupButtonClose.focus()}),popupButtonClose.addEventListener("click",function(){html.classList.remove("popup-open")}),window.addEventListener("keydown",function(o){"Escape"===o.code&&html.classList.contains("popup-open")&&html.classList.remove("popup-open")});
